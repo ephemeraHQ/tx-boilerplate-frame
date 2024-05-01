9eea74693f7794e6f21023be7a9e3a2965f9179d
daria-github <daria@xmtp.com>
2024-05-01 08:30:39 -0700

    update comment

--- src/app/transactionFrames/page.tsx
+++ src/app/transactionFrames/page.tsx
@@ -1,4 +1,4 @@
-// Since Farcaster requires host of redirect link to be same as frame, this interim step is needed.
+// Since the host of redirect link needs to be same as frame, this interim step is needed.
 // Workaround is to redirect to your app, then have your app redirect in the Next.js config.
 export default function RedirectPage() {
   return <h1>Redirecting...</h1>;
