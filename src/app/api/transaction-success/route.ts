1dcb6a164ca9806bc6da04cd322c27df4ae9feff
daria-github <daria@xmtp.com>
2024-05-02 16:06:27 -0700

    update success frame html with helper

--- src/app/api/transaction-success/route.ts
+++ src/app/api/transaction-success/route.ts
@@ -1,3 +1,5 @@
+import { confirmationFrameHtml } from "@/app/page";
+import { getFrameHtmlResponse } from "@coinbase/onchainkit";
 import { getXmtpFrameMessage } from "@coinbase/onchainkit/xmtp";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -10,9 +12,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     return new NextResponse("Message not valid", { status: 500 });
   }
 
-  return new NextResponse(
-    `<!DOCTYPE html><html><head><title>Transaction Success</title><meta property="of:accepts:xmtp" content="2024-02-09"/><meta property="fc:frame" content="vNext"/><meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/og?transaction=0.0000032"/><meta property="fc:frame:button:1" content="Learn more about transaction frames"/><meta property="fc:frame:button:1:action" content="post_redirect"/><meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end"/></head></html>`
-  );
+  return new NextResponse(confirmationFrameHtml);
 }
 export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -26,5 +26,3 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
 export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
-
-export const dynamic = "force-dynamic";
